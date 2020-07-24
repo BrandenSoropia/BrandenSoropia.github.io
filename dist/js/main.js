88fe1be6fedd6ce9a29ea81c858d71342fa3bad7
BrandenSoropia <b.soropia@gmail.com>
2020-07-24 12:59:02 -0400

    Updated what I'm working on and set MVP Done status for Kinpin App. Deleted dist/

--- dist/js/main.js
+++
@@ -1,5 +0,0 @@
-/*! personal-site v1.0.0 | (c) 2020 Branden Soropia | MIT License | https://github.com/BrandenSoropia/personal-site */
-document.addEventListener('click', (function (event) {
-	if (!event.target.matches('#click-me')) return;
-	alert('You clicked me!');
-}), false);
